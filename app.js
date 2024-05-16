f63f06d108583727537c40895e7c634e1be055cc
Antony Xavier <antony.xavier@iinerds.com>
2024-05-16 17:18:29 +0530

    express fix

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var app = express();
 
+const PORT = 3000;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -39,4 +40,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// module.exports = app;
