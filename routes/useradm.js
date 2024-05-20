22931e5f46fdbce71ce3bd663c01a35d1411f563
serina george <serinapinkygeorge@gmail.com>
2024-05-20 19:29:18 +0200

    file writting

--- routes/useradm.js
+++ routes/useradm.js
@@ -3,46 +3,117 @@ const express = require("express");
 var router = express.Router();
 const users = require("../database/user.json");
 
-router.get("/fetchall", (req, res) => {
+
+const fs = require("fs");
+const path = require ("path");
+
+const datafile = path.join(__dirname,'../database/user.json');
+
+//const { error } = require("console");
+router.get("/user", (req, res) => {
+    try{
   return res.status(200).send(users);
+    }
+    catch(error){
+      return  res.send(error);
+    }
 });
 
-router.get("/fetch/:id",(req,res) => {
+router.get("/user/:id",(req,res) => {
+    try{
     const idmatch = users.find((u) => u.userId === parseInt(req.params.id));
     if(!idmatch){
         return res.status(404).send('user not found');
     }
     return res.status(200).send(idmatch);
+}
+catch(error){
+    return res.send(error);
+}
 });
 
-router.post('/adduser',(req,res) => {
-    const{username,password,address} = req.body;
+router.post('/user',(req,res) => {
+    try{
+    const{username,password,address,usertype} = req.body;
 
 const newuser = {
     "userId" : users.length+1,
     "userName": username,
     "password": password,
-    "address" : address
+    "address" : address,
+    "usertype":usertype
 }
 users.push(newuser);
-return res.status(200).send(newuser);
-})
+writedata(users);
 
-router.put("/add/:id",(req,res) => {
-    const{username,password,address} = req.body;
+return res.status(200).send(newuser);
+    }
+    catch(error){
+        return res.send(error);
+    }
+});
+
+router.put("/user/:id",(req,res) => {
+    try{
+    const{username,password,address,usertype} = req.body;
     const idmatch = users.find((u) => u.userId === parseInt(req.params.id));
     //console.log(idmatch);
     //console.log(username);
     idmatch.userName = username;
     idmatch.password = password;
+    idmatch.address = address;
+    idmatch.usertype = usertype;
+    writedata(users);
     return res.status(200).send(idmatch);
+    }
+    catch(error){
+        return res.send(error);
+    }
 });
- router.delete('/delete/:id',(req,res) => {
+ router.delete('/user/:id',(req,res) => {
+    try{
     const index = users.findIndex((u) => u.userId === parseInt(req.params.id));
     if(index>0){
     users.splice(index,1);}
+    writedata(users);
     return res.status(200).send(users);
 
+    } catch(error){
+        return res.send(error) ;
+    }
+    
  });
 
-module.exports = router;
+function writedata(users){
+    const jsonstring = JSON.stringify(users,null,2);
+    fs.writeFile(datafile,jsonstring,(err) => {
+        if (err)
+            {
+              console.log(err.message);
+            }
+            else
+            {
+                console.log('file has been saved ');
+    
+            }
+
+
+
+
+        });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
